fbf14de4aa066ddd5b1e95aa9f2074c81eea634c
Gredwitch <32811753+Gredwitch@users.noreply.github.com>
2018-10-25 20:02:46 +0200

    Removed useless convars

--- ArtiSweps/lua/autorun/client/gred_arti_add.lua
+++ ArtiSweps/lua/autorun/client/gred_arti_add.lua
@@ -1,11 +1,8 @@
 AddCSLuaFile()
 
 local GRED_SVAR = { FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_SERVER_CAN_EXECUTE, FCVAR_NOTIFY }
-CreateConVar("gred_sv_artisweps_spawnaltitude"			, "1000", GRED_SVAR)
-CreateConVar("gred_sv_artisweps_mortarspawnaltitude"	, "700", GRED_SVAR)
 CreateConVar("gred_sv_artisweps_aircrafts"				,  "1"  , GRED_SVAR)
 CreateConVar("gred_sv_artisweps_skybox_mdls"			,  "0"  , GRED_SVAR)
--- CreateClientConVar("gred_sv_artisweps_spawnaltitude"		, "1000" , true,false)
 
 local function gredsettings(CPanel)
 	CPanel:ClearControls()
--- ArtiSweps/lua/autorun/server/gred_arti_add.lua
+++ ArtiSweps/lua/autorun/server/gred_arti_add.lua
@@ -1,8 +1,6 @@
 AddCSLuaFile()
 
 local GRED_SVAR = { FCVAR_REPLICATED, FCVAR_ARCHIVE, FCVAR_SERVER_CAN_EXECUTE, FCVAR_NOTIFY }
-CreateConVar("gred_sv_artisweps_spawnaltitude"			, "1000", GRED_SVAR)
-CreateConVar("gred_sv_artisweps_mortarspawnaltitude"	, "700", GRED_SVAR)
 CreateConVar("gred_sv_artisweps_aircrafts"				,  "1"  , GRED_SVAR)
 CreateConVar("gred_sv_artisweps_skybox_mdls"			,  "0"  , GRED_SVAR)
 
